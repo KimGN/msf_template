c035095850cd50557eb4080fd3706193aaebad68
KimGN <rbska0314@gmail.com>
2020-05-29 18:09:50 +0900

    save

--- public/ko/pc_typeB/src/javascripts/function/main.js
+++ public/ko/pc_typeB/src/javascripts/function/main.js
@@ -1,6 +1,5 @@
 function main() {
     var $speed = 800;
-
     var swiper_visual = new Swiper('.swiper-container.visual_swiper', {
         slidesPerView: "auto",
         speed: $speed,
@@ -16,11 +15,11 @@ function main() {
             prevEl: '.prev_btn.item_btn',
         },
         loop : true,
+        freeMode: true,
         speed: $speed,
         on:{
             init:function(){
                 var $length = $(".item_swiper  .swiper-slide").length;
-                console.log($(".item_box .length"));
                 $(".control_box .length").text($length/3);
                 $(".control_box .counter").text(this.realIndex+1);
             },
@@ -42,7 +41,7 @@ function main() {
             clickable: true,
         },
     });
-    var swiper = new Swiper('.swiper-container.campaigns_swiper', {
+    var swiper_campaigns = new Swiper('.swiper-container.campaigns_swiper', {
         slidesPerView: "auto",
         slidesPerGroup: 2,
         speed: $speed,
@@ -53,37 +52,9 @@ function main() {
             clickable: true,
         }
     });
-    // count
-    function counter() {
-        function numberCounter(target_frame, target_number) {
-            this.count = 0;
-            this.diff = 0;
-            this.target_count = parseInt(target_number);
-            this.target_frame = document.getElementById(target_frame);
-            this.timer = null;
-            this.counter();
-        };
-        numberCounter.prototype.counter = function () {
-            var self = this;
-            this.diff = this.target_count - this.count;
 
-            if (this.diff > 0) {
-                self.count += Math.ceil(this.diff / 5);
-            }
 
-            this.target_frame.innerHTML = this.count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    //
 
-            if (this.count < this.target_count) {
-                this.timer = setTimeout(function () {
-                    self.counter();
-                }, 40);
-            } else {
-                clearTimeout(this.timer);
-            }
-        };
-        new numberCounter("counter3", 11218676);
-        new numberCounter("counter2", 47317);
-        new numberCounter("counter1", 74);
-    }
-    counter();
+
 }
